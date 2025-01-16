df0f6deacb8674edbddb41639daaab50ad31156f
dylangill1921 <156254542+dylangill1921@users.noreply.github.com>
2025-01-16 15:09:36 -0500

    Server Initialized and Database Connection Secured

--- f1-proj/f1-api/app.js
+++ f1-proj/f1-api/app.js
@@ -1,14 +1,20 @@
+// Importing the required modules
 const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
 require('dotenv').config();
 
-
+// Initializing the express app
 const app = express();
 
-// middleware for json bodies
+// Middleware to parse incoming JSON data
 app.use(bodyParser.json());
+
+// Defining the base route for user-related endpoints
 app.use('/api/users', userRoutes);
 
+// The port the server will listen on
 const PORT = process.env.PORT || 3000;
+
+// Listens for incoming requests on the server
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
