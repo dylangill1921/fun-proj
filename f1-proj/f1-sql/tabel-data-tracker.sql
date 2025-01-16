2237d859c6cfd18b940aeb087db62f5a1ad5e149
dylangill1921 <156254542+dylangill1921@users.noreply.github.com>
2025-01-16 11:58:21 -0500

    logging to complete db tables

---
+++ f1-proj/f1-sql/tabel-data-tracker.sql
@@ -0,0 +1,38 @@
+/* Data for all Tables */
+USE Formula1db;
+
+SELECT * FROM articles;
+-- unfinished
+
+SELECT * FROM constructors;
+-- complete
+
+SELECT * FROM countries;
+-- complete
+
+SELECT * FROM drivers;
+-- complete
+
+SELECT * FROM grandPrix;
+-- complete
+
+SELECT * FROM races;
+-- complete
+
+SELECT * FROM seasonsWithConstructors;
+-- unfinished
+
+SELECT * FROM raceStats;
+-- unfinished
+
+SELECT * FROM subscriptions;
+-- complete
+
+SELECT * FROM tireCompounds;
+-- unfinished
+
+SELECT * FROM tireTypes;
+-- unfinished
+
+SELECT * FROM tracks;
+-- complete
