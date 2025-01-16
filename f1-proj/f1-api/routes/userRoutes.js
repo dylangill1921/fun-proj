825009c8a1d6cc1386ff2c32cc9b4a1542aeceb6
dylangill1921 <156254542+dylangill1921@users.noreply.github.com>
2025-01-16 15:11:49 -0500

    Established the Users Routes

--- f1-proj/f1-api/routes/userRoutes.js
+++ f1-proj/f1-api/routes/userRoutes.js
@@ -1,9 +1,15 @@
 // routes/userRoutes.js
+
+// Import the required modules
 const express = require('express');
 const router = express.Router();
 const {registerUser, loginUser} = require('../controllers/userController');
 
+// Route for registering a new user
 router.post('/register', registerUser);
+
+// Route for a new login
 router.post('/login', loginUser);
 
+// Export the router to be used in the app
 module.exports = router;
