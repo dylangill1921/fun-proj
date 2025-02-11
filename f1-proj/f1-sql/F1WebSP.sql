d1a761f81a83904bab26e9eb0bd5541b3f0055c5
dylangill1921 <156254542+dylangill1921@users.noreply.github.com>
2025-02-11 13:57:04 -0500

    Adjusted the API

--- f1-proj/f1-sql/F1WebSP.sql
+++ f1-proj/f1-sql/F1WebSP.sql
@@ -2,7 +2,53 @@
 USE Formula1db;
 
 /* Register a user */
+CREATE PROCEDURE spRegisterNewUser
+    @StatusID INT,
+    @firstName VARCHAR(50),
+    @lastName VARCHAR(50),
+    @userName VARCHAR(50),
+    @password VARCHAR(255),
+    @email VARCHAR(255),
+    @phoneNumber VARCHAR(20),
+    @dateOfBirth DATE,
+    @country CHAR(3),
+    @favouriteDriver VARCHAR(100),
+    @favouriteConstructor VARCHAR(100)
+AS
+BEGIN
+    SET NOCOUNT ON;
 
+    -- Insert the new user into the 'users' table
+    INSERT INTO users (
+        StatusID,
+        firstName,
+        lastName,
+        userName,
+        password,
+        email,
+        phoneNumber,
+        dateOfBirth,
+        country,
+        favouriteDriver,
+        favouriteConstructor
+    )
+    VALUES (
+        @StatusID,
+        @firstName,
+        @lastName,
+        @userName,
+        @password,
+        @email,
+        @phoneNumber,
+        @dateOfBirth,
+        @country,
+        @favouriteDriver,
+        @favouriteConstructor
+    );
+
+    -- Return the newly created user's ID
+    SELECT SCOPE_IDENTITY() AS UserID;
+END;
 
 /* Login a user */
 
